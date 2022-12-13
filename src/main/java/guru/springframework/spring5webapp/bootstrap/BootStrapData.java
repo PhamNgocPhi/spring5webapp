9028eae684869ed42c187e346e4480a2523cde82
PhamNgocPhi <phiphongmta@gmail.com>
2022-12-13 22:57:58 +0700

    add pojo and repository

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +14,12 @@ public class BootStrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -23,20 +27,30 @@ public class BootStrapData implements CommandLineRunner {
 
         Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("DDD", "1234");
+        Publisher publisher = new Publisher("Puaaa", "fssdfds","kkk","oo","ppp");
+        publisherRepository.save(publisher);
+
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
+        ddd.setPublisher(publisher);
+        publisher.getBooks().add(ddd);
         authorRepository.save(eric);
         bookRepository.save(ddd);
 
-        Author eric1 = new Author("Eric1", "Evans1");
-        Book ddd1 = new Book("DDDBFD", "1234dd");
-        eric.getBooks().add(ddd1);
-        ddd.getAuthors().add(eric1);
-        authorRepository.save(eric1);
-        bookRepository.save(ddd1);
+        Author author = new Author("Eric1", "Evans1");
+        Book book = new Book("DDDBFD", "1234dd");
+        author.getBooks().add(book);
+        book.getAuthors().add(author);
+        book.setPublisher(publisher);
+        publisher.getBooks().add(book);
+        authorRepository.save(author);
+        bookRepository.save(book);
+
+
 
         System.out.println("stared bootstrap");
         System.out.println("book count: " + bookRepository.count());
+        System.out.println("publisher number of books: " + publisher.getBooks().size());
 
     }
 }
